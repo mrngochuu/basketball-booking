ae733b56512c154e9e62ee8cc91b4c33953a637e
NgocHuu <ngochuu.bts@gmail.com>
2020-12-08 15:56:58 +0700

    [HuuDN] - build

--- src/main/java/com/basketballbooking/BasketballBookingApplication.java
+++ src/main/java/com/basketballbooking/BasketballBookingApplication.java
@@ -3,11 +3,14 @@ package com.basketballbooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class BasketballBookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasketballBookingApplication.class, args);
 	}
-
+	
 }
